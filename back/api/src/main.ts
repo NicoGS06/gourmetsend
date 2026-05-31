4a289b83afca4a3233cd145f8bce7b5d7ab28564
nicolas <nicolasgutierrezs2006@gmail.com>
2026-05-30 21:42:27 -0500

    intento de arreglo ya mil y una vez

--- back/api/src/main.ts
+++ back/api/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3001;
 
-  await app.listen(port, '0.0.0.0');
-  console.log(`🚀 Servidor ejecutándose en: http://localhost:${port}/api`);
+await app.listen(port, '0.0.0.0');
+console.log(`🚀 Servidor ejecutándose en: http://0.0.0.0:${port}/api`);
 }
 bootstrap();
